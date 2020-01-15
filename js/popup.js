3ce2c592c233c051464e790ec0d3a62c63469490
burda-a-m <burda_a_m@rambler.ru>
2020-01-15 23:04:42 +0300

    Запрограммировал 'ушки' слайдера, поправил стили для улучшения перфект-пикселя, поправил окно с формой и минимизировал файлы со стилями и скриптами

--- js/popup.js
+++ js/popup.js
@@ -35,7 +35,7 @@
 
     if ((popup_basket)&&(buttons_buy_arr)) {
       for (var i = 0; i < buttons_buy_arr.length; i++) {
-        buttons_buy_arr[i].addEventListener('click', function (evt) {
+        buttons_buy_arr[i].addEventListener("click", function (evt) {
           evt.preventDefault();
           overlay.classList.add("show");
           popup_basket.classList.add("popup-show");
@@ -71,7 +71,7 @@
       isStorageEmail = false;
     }
 
-    if ((btn_feedback)&&(btn_feedback)) {
+    if ((btn_feedback)&&(popup_feedback)) {
       // Открываем popup-окно с формой обратной связи
       btn_feedback.addEventListener("click", function (evt) {
         evt.preventDefault();
@@ -89,7 +89,6 @@
 
       // Обработка формы обратной связи при щелчке по кнопке "Отправить"
       form.addEventListener("submit", function (evt) {
-        popup_feedback.classList.remove("popup-error");
         if ( (!field_name.value) ||
               (!field_email.value) ||
               (!field_message.value)
@@ -104,6 +103,7 @@
           } else if (!field_message.value) {
             field_message.focus();
           }
+          popup_feedback.classList.remove("popup-error");
           popup_feedback.classList.add("popup-error");
         } else {
           // Все поля заполнены. Сохраним их значения в локальное хранилище
@@ -128,10 +128,8 @@
     function popup_close(evt) {
       evt.preventDefault();
 
-      var popup, i;
-
-      for (i = 0; i < popup_arr.length; i++) {
-        popup = popup_arr[i];
+      for (var i = 0; i < popup_arr.length; i++) {
+        var popup = popup_arr[i];
         overlay.classList.remove("show");
         popup.classList.remove("popup-show");
         popup.classList.remove("popup-error");
@@ -145,24 +143,83 @@
     // Массив из "крестиков" (кнопок закрытия popup-окон)
     var button_close_arr = document.querySelectorAll(".btn-popup-close");
 
-    var button_close, i;
-    
-    for (i = 0; i < button_close_arr.length; i++) {
-      button = button_close_arr[i];
-      button.addEventListener('click', popup_close);
+    for (var i = 0; i < button_close_arr.length; i++) {
+      var button = button_close_arr[i];
+      button.addEventListener("click", popup_close);
     }
     // /-
 
 
     // Вешаю функцию popup_close на щелчок по оверлею (Области вне popup-окна)
-    overlay.addEventListener('click', popup_close);
+    overlay.addEventListener("click", popup_close);
     // /-
 
 
     // Вешаю функцию popup_close на нажатие клавиши Esc
     window.addEventListener("keydown", function(evt) {
-      if (evt.keyCode===27) {
+      if (evt.keyCode === 27) {
         popup_close(evt);
       }
     });
     // /-
+
+
+
+
+
+/*
+ *  Оживляю "ушки" слайдера
+ */
+
+    // массив радиокнопок для переключателей
+    var slider_check_arr = document.querySelectorAll(".slider > input[type=radio]");
+
+    // "Ушки" (левое и правое соответственно)
+    var arrow_left = document.querySelector(".slider > .arrow-left");
+    var arrow_right = document.querySelector(".slider > .arrow-right");
+
+    // Функция, вычисляющая номер активного слайда, а он должен быть обязательно
+    function slide_active_number() {
+      var i = 0;
+      while ( (i < slider_check_arr.length) && (!slider_check_arr[i].checked) ) {
+        i++;
+      }
+          // i вычисленно, но оно на единицу меньше, т.к. ииндексы массивов начинаются с нуля.
+          // Поэтому функции передаю значение на единицу больше
+      return (i+1);
+    }
+    // /-
+
+    // Функция-процедура, активирующая нужную радиокнопку в слайдере
+    function activate_slide_radiobutton(number) {
+      var radiobutton_id = "#slider-check-" + number;
+      var radiobutton = document.querySelector(radiobutton_id);
+      radiobutton.checked = true;
+    }
+
+    if (slider_check_arr && arrow_left && arrow_right) {
+      // В эту переменную запишу номер слайда, к которому надо перейти
+      var new_number;
+
+      // Вешаю событие на левое "ушко"
+      arrow_left.addEventListener("click", function() {
+        var active_number = slide_active_number();
+        if ( active_number > 1 ) {
+          new_number = active_number - 1;
+        } else {
+          new_number = slider_check_arr.length;
+        }
+        activate_slide_radiobutton(new_number);
+      });
+
+      // Вешаю событие на правое "ушко"
+      arrow_right.addEventListener("click", function() {
+        var active_number = slide_active_number();
+        if ( active_number < slider_check_arr.length) {
+          new_number = active_number + 1;
+        } else {
+          new_number = 1;
+        }
+        activate_slide_radiobutton(new_number);
+      });
+    }
